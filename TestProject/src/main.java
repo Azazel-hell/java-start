4d82019df09789f48b3b65881f8f8fc8d7024ecc
Azazel-hell <free.shok2@gmail.com>
2022-01-28 12:38:39 +0200

    add all project file

--- TestProject/src/main.java
+++ TestProject/src/main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class main {
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Name of person is " + name);
+    }
 }

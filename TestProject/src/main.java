b307c2a2a9fde8c42d1f88bd22829e396d5671ae
Azazel-hell <free.shok2@gmail.com>
2022-01-28 12:52:37 +0200

    update project files

--- TestProject/src/main.java
+++ TestProject/src/main.java
@@ -4,6 +4,8 @@ public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
+        var age = scanner.nextInt();
         System.out.println("Name of person is " + name);
+        System.out.println("Age person is " + age);
     }
 }
